b0146c5f50420f38a777720314b278b14fb2f1fe
Sergey Kasmy <SleeplessSloth79@gmail.com>
2021-07-17 23:05:59 +0200

    Add a primitive menu

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,18 @@
+struct Players(Vec<Player>);
+
 #[derive(Debug)]
 struct Player {
     name: String,
     class: String,
     skills: Vec<Skill>,
-    status: Vec<Status>,
+    statuses: Vec<Status>,
     money: u32
 }
 
 #[derive(Debug)]
 struct Skill {
     name: String,
-    moves_unavailable: u8,
+    available_after: u8,
 }
 
 #[derive(Debug)]
@@ -21,12 +23,73 @@ enum StatusType {
 
 #[derive(Debug)]
 struct Status {
-    status: StatusType,
+    status_type: StatusType,
     duration: u8,
 }
 
-pub fn run() {
-    let test = Player { name: String::from("Ciren"), class: String::from("Полу-человек полу-эльф"), skills: vec![ Skill { name: String::from("Test Skill 1"), moves_unavailable: 0 }, Skill { name: String::from("Test Skill 2"), moves_unavailable: 0 } ], status: vec![Status { status: StatusType::Luck, duration: 2 }], money: 50 };
+// clear terminal and position the cursor at 0,0
+fn clear_screen() {
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+}
 
-    println!("{:?}", test);
+fn print_player(player: &Player) {
+    println!("Name: {}", player.name);
+    println!("Class: {}", player.class);
+
+    println!("Skills:");
+    for skill in &player.skills {
+        println!("....Skill: {}, Available after: {} moves", skill.name, skill.available_after);
+    }
+
+    println!("Statuses:");
+    for status in &player.statuses {
+        println!("....Status: {:?}, Still active for {} moves", status.status_type, status.duration);
+    }
+
+    println!("Money: {}", player.money);
+}
+
+pub fn run() {
+    let player = Player { name: String::from("Ciren"), class: String::from("Полу-человек полу-эльф"), skills: vec![ Skill { name: String::from("Test Skill 1"), available_after: 0 }, Skill { name: String::from("Test Skill 2"), available_after: 0 } ], statuses: vec![Status { status_type: StatusType::Luck, duration: 2 }], money: 50 };
+    let players = Players(vec![player]);
+    loop {
+        clear_screen();
+        println!("1. Start game");
+        println!("2. View/Edit characters");
+        println!("q. Quit");
+
+        let sin = std::io::stdin();
+        let mut input = String::new();
+        sin.read_line(&mut input).unwrap();
+
+        let input = input.trim();
+        match input {
+            "2" => character_menu(Some(&players)),
+            "q" => break,
+            _ => ()
+        }
+    }
+}
+
+fn character_menu(players: Option<&Players>) {
+    loop {
+        clear_screen();
+        if let Some(players) = players {
+            for player in &players.0 {
+                print_player(&player);
+            }
+        } else {
+            println!("There are no players.");
+        }
+
+        let sin = std::io::stdin();
+        let mut input = String::new();
+        sin.read_line(&mut input).unwrap();
+
+        let input = input.trim();
+        match input {
+            "q" => return,
+            _ => (),
+        }
+    }
 }
